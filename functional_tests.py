516bd32c00b8d3695d927de340b3ff6eec7030a1
sjung <sjung2393@gmail.com>
2016-12-28 19:42:38 +0700

    Add helper method, FT refactor

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Claires heard of a new to-do web app. She goes to the site
         # to check it out.
@@ -34,21 +39,29 @@ class NewVisitorTest(unittest.TestCase):
         # She hits enter, and upon doing so, the page updates and now
         # lists: "1: Look for a post-office"
         inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Look for a post-office')
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Look for a post-office', [row.text for row in rows])
-        self.assertIn(
-        '2: Use peacock feathers to make a fly' ,
-         [row.text for row in rows]
-    )
+
+    #     table = self.browser.find_element_by_id('id_list_table')
+    #     rows = table.find_elements_by_tag_name('tr')
+    #     self.assertIn('1: Look for a post-office', [row.text for row in rows])
+    #     self.assertIn(
+    #     '2: Use peacock feathers to make a fly' ,
+    #      [row.text for row in rows]
+    # )
 
         # The text-box remains for her to input another item. She enters:
         # "Ask Kris to send over go-pro"
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Ask Kris to send over go-pro')
+        inputbox.send_keys(Keys.ENTER)
+
 
         # The page updates a second time and both her items are still
         # on the list
+        self.check_for_row_in_list_table('1: Look for a post-office')
+        self.check_for_row_in_list_table('2: Ask Kris to send over go-pro')
+        self.fail('Finish the test!')
 
         # Claire notices that the site has generated a unique URL for her.
         # Along with explanatory text.
