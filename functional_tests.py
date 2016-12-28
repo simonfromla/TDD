4e1e1036ed6de31ac5c79269a306fc9075ad77da
sjung <sjung2393@gmail.com>
2016-12-28 16:18:10 +0700

    FT checks for to-do item input

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -17,17 +18,32 @@ class NewVisitorTest(unittest.TestCase):
 
         # She notices the page title and header mention the To-Do list
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # She's prompted to enter a to-do item and test the app herself
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # She types in "Look for a post-office" into a text box
+        inputbox.send_keys('Look for a post-office')
 
         # She hits enter, and upon doing so, the page updates and now
         # lists: "1: Look for a post-office"
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Look for a post-office' for row in rows)
+        )
 
         # The text-box remains for her to input another item. She enters:
         # "Ask Kris to send over go-pro"
+        self.fail('Finish the test!')
 
         # The page updates a second time and both her items are still
         # on the list
