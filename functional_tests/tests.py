9473e2f7b058c0797cdc3a4023cbf5d870f581ed
sjung <sjung2393@gmail.com>
2017-01-03 15:34:47 +0700

    Make FT into app. Using LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,9 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 import time
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -10,7 +11,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self):
         self.browser.quit()
-
+# Put helper methods near top class, btwn teardown and first test
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
@@ -19,7 +20,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Claires heard of a new to-do web app. She goes to the site
         # to check it out.
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention the To-Do list
         self.assertIn('To-Do', self.browser.title)
@@ -40,7 +41,6 @@ class NewVisitorTest(unittest.TestCase):
         # lists: "1: Look for a post-office"
         inputbox.send_keys(Keys.ENTER)
         self.check_for_row_in_list_table('1: Look for a post-office')
-
 
     #     table = self.browser.find_element_by_id('id_list_table')
     #     rows = table.find_elements_by_tag_name('tr')
