23b62e1794a6b6f406b1defb772e425f14b54c7d
sjung <sjung2393@gmail.com>
2017-01-06 18:44:40 +0700

    Hack FT runner to be able to test staging site

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,11 +1,21 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import sys
 import unittest
 import time
 
 
 class NewVisitorTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -22,7 +32,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def test_can_start_a_list_for_one_user(self):
         # Claires heard of a new to-do web app. She goes to the site
         # to check it out.
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         # She notices the page title and header mention the To-Do list
         self.assertIn('To-Do', self.browser.title)
@@ -60,7 +70,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Claire starts a new todo list
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Look for a post-office')
 
@@ -81,7 +91,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         # Francis visits the home page.  There is no sign of Claire's
         # list
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Look for a post-office', page_text)
@@ -107,7 +117,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_layout_and_styling(self):
         # Claire goes to the home page
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         # She notices the input box is nicely centered
