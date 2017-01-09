3e2caf25cd534bb45b1c6f90d058d869857fc926
sjung <sjung2393@gmail.com>
2017-01-09 14:23:57 +0700

    Settings.py edit

--- superlists/settings.py
+++ superlists/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-from private_settings import *
+from . import private_settings
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
