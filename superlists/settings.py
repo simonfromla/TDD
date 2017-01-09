5e4d4e57c7fa4e91865bf0a4db046230e411aa53
sjung <sjung2393@gmail.com>
2017-01-09 14:26:06 +0700

    rm psettings

--- superlists/settings.py
+++ superlists/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-from . import private_settings
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = private_settings.KEY
+SECRET_KEY = 'v2&vv&&mtb1mr@iuipsiki(e9q6#u!60k!i648b$f0t4e*-jif'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
